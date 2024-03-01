2e0ed2654936efc78d7d4c5d5c940d1985cb385a
aydosjs <aji3385@gmail.com>
2024-03-01 13:01:27 +0500

    comments UI

--- components/CommentItem.tsx
+++ components/CommentItem.tsx
@@ -1,13 +1,13 @@
 export default function CommentItem() {
   return (
-    <div className="w-full flex flex-row space-x-3 dark:border-slate-700">
+    <div className="w-full flex flex-row space-x-3 dark:border-slate-700 group">
       <div>
         <span className="size-8 rounded-full bg-sky-500 flex items-center justify-center p-2">
           J
         </span>
       </div>
       <div className="py-0 w-full">
-        <p className="text-slate-500 dark:text-slate-500 text-[.9rem] font-normal">
+        <p className="dark:group-hover:text-slate-400 group-hover:text-slate-600 transition-all duration-300 text-slate-500 dark:text-slate-500 text-[.9rem] font-normal">
           <span className="inline-block text-slate-900 dark:text-slate-100 mr-2 font-medium">
             John Dev
           </span>
@@ -16,19 +16,19 @@ export default function CommentItem() {
           Incidunt, iste!
         </p>
 
-        <div className="mt-2 flex flex-row space-x-4">
+        <div className="flex flex-row space-x-4 mt-1">
           <div>
             <span className="text-slate-500 hover:text-slate-900 dark:hover:text-slate-100 dark:text-slate-500 cursor-pointer text-sm font-normal">
               Like
             </span>
           </div>
           <div>
-            <span className="text-slate-500 hover:text-slate-900 dark:hover:text-slate-100 dark:text-slate-500 cursor-pointer text-sm font-normal">
+            <span className="text-slate-500 dark:text-slate-500 text-sm font-normal">
               2 days
             </span>
           </div>
           <div>
-            <span className="text-slate-500 hover:text-slate-900 dark:hover:text-slate-100 dark:text-slate-500 cursor-pointer text-sm font-normal">
+            <span className="text-slate-500 dark:text-slate-500 text-sm font-normal">
               43 likes
             </span>
           </div>
