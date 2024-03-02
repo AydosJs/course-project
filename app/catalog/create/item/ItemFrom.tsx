538a60dca4f50e60a71424dab0f1271b8a2e41d4
aydosjs <aji3385@gmail.com>
2024-03-02 12:48:25 +0500

    tags autocomplate input

---
+++ app/catalog/create/item/ItemFrom.tsx
@@ -0,0 +1,83 @@
+"use client";
+import Button from "@/components/form-elements/Button";
+import Input from "@/components/form-elements/Input";
+import { useState } from "react";
+import TagsInput from "./TagsInput";
+
+export default function ItemForm() {
+  const [formData, setFormData] = useState<CatalogType>({
+    title: "",
+    description: "",
+  });
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+      <Input
+        label="Title"
+        id="title"
+        name="title"
+        value={formData.title}
+        onChange={handleChange}
+      />
+
+      <div>
+        <label
+          htmlFor="description"
+          className="block mb-1 font-medium text-sm text-slate-600 dark:text-slate-500 leading-6"
+        >
+          Description
+        </label>
+        <textarea
+          value={formData.description}
+          onChange={handleChange}
+          id="description"
+          name="description"
+          className="w-full bg-slate-100 peer text-slate-900 dark:text-slate-400 dark:bg-slate-800 dark:border-slate-700 dark:placeholder:text-slate-500 placeholder:text-slate-400 font-medium p-2 border-2 outline-none dark:focus:border-slate-600 focus:border-slate-400 text-sm rounded "
+          rows={3}
+          placeholder="Add a comment..."
+        />
+      </div>
+
+      <div>
+        <label
+          htmlFor="tags"
+          className="block mb-1 font-medium text-sm text-slate-600 dark:text-slate-500 leading-6"
+        >
+          Tags
+        </label>
+
+        <TagsInput />
+      </div>
+
+      <div className="flex flex-row space-x-6">
+        <Button
+          type="submit"
+          className=" bg-slate-600 hover:bg-slate-700 dark:bg-slate-700 py-3 dark:hover:bg-slate-700/50 border-none"
+        >
+          Cancel
+        </Button>
+        <Button
+          type="submit"
+          className="border-none bg-sky-500 py-3 hover:bg-sky-600"
+        >
+          Create
+        </Button>
+      </div>
+    </form>
+  );
+}
---
+++ app/catalog/create/item/TagsInput.tsx
@@ -0,0 +1,107 @@
+import { cn } from "@/lib/utils/utils";
+import CreatableSelect from "react-select/creatable";
+
+const multiValueStyles =
+  "bg-sky-500 rounded items-center py-0 text-sm text-slate-900 dark:text-slate-200 pl-2 pr-2 gap-1.5 mr-1";
+const multiValueLabelStyles = "leading-6 py-0.5";
+
+const controlStyles = {
+  base: "p-1 px-2 rounded bg-slate-100 dark:bg-slate-800 dark:text-slate-200 text-slate-600 font-medium text-sm border-2 dark:border-slate-700  outline-none",
+  focus: "dark:border-slate-600 border-slate-400",
+  nonFocus: "outline-none",
+};
+const placeholderStyles = "dark:text-slate-500 text-slate-400 p-2 px-2";
+const selectInputStyles = "pl-2 py-2";
+const valueContainerStyles = "";
+const indicatorsContainerStyles = "";
+const indicatorSeparatorStyles = "bg-neutral-600 hidden";
+const dropdownIndicatorStyles = "px-2 text-neutral-400";
+
+const menuStyles =
+  "p-1 mt-2 border-2 dark:border-slate-700 text-sm bg-slate-50 dark:bg-slate-800 rounded";
+const singleValueStyles = "px-2";
+const optionStyles = {
+  base: "hover:cursor-pointer p-2 py-2 rounded dark:hover:bg-slate-700/50 hover:bg-slate-200 dark:text-slate-200 text-slate-600 font-medium first:mt-0 mt-1",
+  focus: "dark:bg-slate-700/50",
+  selected: "dark:bg-slate-700 bg-slate-400",
+};
+
+const groupHeadingStyles = "ml-3 mt-2 mb-1 text-gray-500 text-sm";
+const noOptionsMessageStyles =
+  "text-gray-500 p-2 bg-gray-50 border border-dashed border-gray-200 rounded-sm";
+
+export type ReactSelectTypes = {
+  value: string;
+  label: string;
+};
+
+export type OnChangeFunction = (value: ReactSelectTypes) => void;
+
+const TagsInput = (props: any) => {
+  return (
+    <CreatableSelect
+      placeholder="Add tags"
+      components={{
+        IndicatorSeparator: () => null,
+        DropdownIndicator: () => null,
+      }}
+      isMulti
+      options={[
+        {
+          value: "tag1",
+          label: "Game",
+        },
+        {
+          value: "tag2",
+          label: "Movie",
+        },
+      ]}
+      unstyled
+      styles={{
+        input: (base) => ({
+          ...base,
+          "input:focus": {
+            boxShadow: "none",
+          },
+        }),
+        multiValueLabel: (base) => ({
+          ...base,
+          whiteSpace: "normal",
+          overflow: "visible",
+        }),
+        control: (base) => ({
+          ...base,
+          transition: "none",
+        }),
+      }}
+      classNames={{
+        control: ({ isFocused }) =>
+          cn(
+            isFocused ? controlStyles.focus : controlStyles.nonFocus,
+            controlStyles.base
+          ),
+        placeholder: () => placeholderStyles,
+        input: () => selectInputStyles,
+        valueContainer: () => valueContainerStyles,
+        singleValue: () => singleValueStyles,
+        multiValue: () => multiValueStyles,
+        multiValueLabel: () => multiValueLabelStyles,
+        indicatorsContainer: () => indicatorsContainerStyles,
+        indicatorSeparator: () => indicatorSeparatorStyles,
+        dropdownIndicator: () => dropdownIndicatorStyles,
+        menu: () => menuStyles,
+        groupHeading: () => groupHeadingStyles,
+        option: ({ isFocused, isSelected }) =>
+          cn(
+            isFocused && optionStyles.focus,
+            isSelected && optionStyles.selected,
+            optionStyles.base
+          ),
+        noOptionsMessage: () => noOptionsMessageStyles,
+      }}
+      {...props}
+    />
+  );
+};
+
+export default TagsInput;
