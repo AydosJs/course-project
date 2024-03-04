9ec386b3c4175dd9d962c552e8ee64e90459b3ee
aydosjs <aji3385@gmail.com>
2024-03-04 21:51:06 +0500

    colors

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -16,86 +16,89 @@ export default async function Home({
   const { t } = await initTranslations(locale, ["default"]);
 
   return (
-    <main className="my-10 min-h-[calc(100vh-108px)] flex flex-col items-center justify-between lg:p-24">
-      <div className="w-full flex flex-col items-center justify-center">
-        <Search />
+    <>
+      <div className="absolute inset-0 bottom-0  left-0  right-0 top-0 -z-10 bg-[linear-gradient(to_right,#0ea5e9_1px,transparent_1px),linear-gradient(to_bottom,#0ea5e9_1px,transparent_1px)] bg-[size:64px_64px] opacity-10 [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_140%)]"></div>
 
-        <div className="container max-w-7xl w-full mt-20 flex flex-col space-y-20">
-          <div className="mt-20">
-            <h1 className="mb-8 text-2xl dark:text-slate-400 text-slate-900 font-medium">
-              {t("top_5__most_collections")}
-            </h1>
-            <div className="lg:mt-0 w-full h-fit grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-4">
-              <Link href={`/catalog/1/1`}>
-                <CollectionItem />
-              </Link>
-              <Link href={`/catalog/2/1`}>
-                <CollectionItem />
-              </Link>
-              <Link href={`/catalog/3/1`}>
-                <CollectionItem />
-              </Link>
-              <Link href={`/catalog/4/1`}>
-                <CollectionItem />
-              </Link>
-              <Link href={`/catalog/1/1`}>
-                <CollectionItem />
-              </Link>
-              <Link href={`/catalog/2/1`}>
-                <CollectionItem />
-              </Link>
-              <Link href={`/catalog/3/1`}>
-                <CollectionItem />
-              </Link>
-              <Link href={`/catalog/4/1`}>
-                <CollectionItem />
-              </Link>
+      <Search />
+      <main className="my-10 flex min-h-[calc(100vh-108px)] flex-col items-center justify-between lg:py-12">
+        <div className="flex w-full flex-col items-center justify-center">
+          <div className="container flex w-full max-w-7xl flex-col space-y-20">
+            <div>
+              <h1 className="mb-8 text-2xl font-medium text-slate-900 dark:text-slate-400">
+                {t("top_5__most_collections")}
+              </h1>
+              <div className="grid h-fit w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:mt-0 lg:grid-cols-2 xl:grid-cols-4">
+                <Link href={`/catalog/1/1`}>
+                  <CollectionItem />
+                </Link>
+                <Link href={`/catalog/2/1`}>
+                  <CollectionItem />
+                </Link>
+                <Link href={`/catalog/3/1`}>
+                  <CollectionItem />
+                </Link>
+                <Link href={`/catalog/4/1`}>
+                  <CollectionItem />
+                </Link>
+                <Link href={`/catalog/1/1`}>
+                  <CollectionItem />
+                </Link>
+                <Link href={`/catalog/2/1`}>
+                  <CollectionItem />
+                </Link>
+                <Link href={`/catalog/3/1`}>
+                  <CollectionItem />
+                </Link>
+                <Link href={`/catalog/4/1`}>
+                  <CollectionItem />
+                </Link>
+              </div>
             </div>
-          </div>
-          <div>
-            <h1 className="mb-8 text-2xl dark:text-slate-400 text-slate-900 font-medium">
-              {t("top_5__most_collections")}
-            </h1>
-            <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-6 gap-y-4 md:gap-4 w-full">
-              {new Array(5).fill(" ").map((i, index) => (
-                <Link
-                  className="col-span-2 md:col-span-1 xl:col-span-2"
-                  href={`/catalog/${index + 1}`}
-                  key={index}
-                >
-                  <CatalogCard
-                    title="Museum"
-                    description="Imagine telling your collection’s stories by cataloging,
+            <div>
+              <h1 className="mb-8 text-2xl font-medium text-slate-900 dark:text-slate-400">
+                {t("top_5__most_collections")}
+              </h1>
+              <div className="grid w-full grid-cols-1 gap-y-4 md:grid-cols-3 md:gap-4 xl:grid-cols-6">
+                {new Array(5).fill(" ").map((i, index) => (
+                  <Link
+                    className="col-span-2 md:col-span-1 xl:col-span-2"
+                    href={`/catalog/${index + 1}`}
+                    key={index}
+                  >
+                    <CatalogCard
+                      title="Museum"
+                      description="Imagine telling your collection’s stories by cataloging,
                    maintaining, and sharing their rich histories. Streamline
                    operations, empower your staff, and visually explore your
                    objects."
-                  />
-                </Link>
-              ))}
+                    />
+                  </Link>
+                ))}
 
-              <Link
-                className="col-span-2 md:col-span-1 xl:col-span-2"
-                href="/auth/login"
-              >
-                <div
-                  className={`col-span-2 md:col-span-1 xl:col-span-2 flex flex-col justify-between relative cursor-pointer bg-sky-300/10 dark:bg-sky-600/10 dark:hover:border-sky-500 transition-all duration-300 group rounded border-2 border-sky-600/20 p-4 min-h-60`}
+                <Link
+                  className="col-span-2 md:col-span-1 xl:col-span-2"
+                  href="/auth/login"
                 >
-                  <div className="absolute inset-0 -z-10 h-full group-hover:opacity-20 duration-300 transition-all opacity-10 w-full bg-[linear-gradient(to_right,#0ea5e9_1px,transparent_1px),linear-gradient(to_bottom,#0ea5e9_1px,transparent_1px)] bg-[size:6.6rem_4.6rem] group-hover:bg-[size:7.6rem_5.6rem]"></div>
+                  <div
+                    className={`group relative col-span-2 flex min-h-60 cursor-pointer flex-col justify-between rounded border-2 border-sky-600/20 bg-sky-300/10 p-4 transition-all duration-300 dark:bg-sky-600/10 dark:hover:border-sky-500 md:col-span-1 xl:col-span-2`}
+                  >
+                    <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#0ea5e9_1px,transparent_1px),linear-gradient(to_bottom,#0ea5e9_1px,transparent_1px)] bg-[size:6.6rem_4.6rem] opacity-10 transition-all duration-300 group-hover:bg-[size:7.6rem_5.6rem] group-hover:opacity-20"></div>
 
-                  <p className="opacity-0 transition-all duration-300 text-sky-500 dark:text-sky-600  dark:group-hover:text-sky-400 font-semibold text-2xl group-hover:opacity-100">
-                    CLICK IT!
-                  </p>
-                  <p className="text-sky-500 dark:text-sky-600  dark:group-hover:text-sky-400 transition-all duration-300 text-[2.5rem] sm:text-[2.6rem] underline leading-snug font-semibold">
-                    {t("login_title")}
-                  </p>
-                </div>
-              </Link>
+                    <p className="text-2xl font-semibold text-sky-500 opacity-0 transition-all  duration-300 group-hover:opacity-100 dark:text-sky-600 dark:group-hover:text-sky-400">
+                      CLICK IT!
+                    </p>
+                    <p className="text-[2.5rem] font-semibold  leading-snug text-sky-500 underline transition-all duration-300 dark:text-sky-600 dark:group-hover:text-sky-400 sm:text-[2.6rem]">
+                      {t("login_title")}
+                    </p>
+                  </div>
+                </Link>
+              </div>
             </div>
-          </div>
 
-          <TagsList />
+            <TagsList />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
--- components/Search.tsx
+++ components/Search.tsx
@@ -4,28 +4,32 @@ import Badge from "./Badge";
 
 export default function Search() {
   return (
-    <div className="mb-16 w-full p-[1rem] transition-all duration-300 lg:max-w-3xl lg:focus-within:max-w-4xl">
-      <Input
-        className="w-full rounded-full border-sky-200 bg-white !p-4 font-normal text-sky-500/95 placeholder:text-sky-300 focus:border-sky-300 dark:border-sky-500/30 dark:bg-sky-500/5 dark:text-sky-500/90 dark:placeholder:text-sky-500/50 dark:focus:border-sky-500/50 lg:text-[1rem] "
-        placeholder="Full-text search..."
-      />
+    <div className="relative mt-20 flex h-full w-full items-center justify-center py-20">
+      <div className="mb-16 w-full p-[1rem] transition-all duration-300 lg:max-w-3xl lg:focus-within:max-w-4xl">
+        <Input
+          className="w-full rounded-full border-sky-200 bg-white !p-4 font-normal text-sky-500/95 backdrop-blur-sm backdrop-filter placeholder:text-sky-300 focus:border-sky-300  dark:border-sky-500/30 dark:bg-sky-500/5 dark:text-sky-500/90 dark:placeholder:text-sky-500/50 dark:focus:border-sky-500/50 lg:text-[1rem] "
+          placeholder="Full-text search..."
+        />
 
-      <div className="mt-3 flex flex-row flex-wrap gap-2">
-        {[
-          "#Adventure",
-          "#Vintage",
-          "#Abstract",
-          "#Minimalist",
-          "#Fantasy",
-          "#Technology",
-        ].map((item: string, index: number) => (
-          <Badge
-            className="border-sky-200 bg-white font-normal text-sky-500 opacity-80 transition-all duration-300 hover:border-sky-200 hover:bg-white hover:text-sky-600 hover:opacity-100"
-            title={item}
-            key={index}
-          />
-        ))}
+        <div className="mt-3 flex flex-row flex-wrap gap-2">
+          {[
+            "#Adventure",
+            "#Vintage",
+            "#Abstract",
+            "#Minimalist",
+            "#Fantasy",
+            "#Technology",
+          ].map((item: string, index: number) => (
+            <Badge
+              className="border-sky-200 bg-white font-normal text-sky-500 opacity-80 backdrop-blur-sm backdrop-filter transition-all duration-300 hover:border-sky-200 hover:bg-white hover:text-sky-600 hover:opacity-100"
+              title={item}
+              key={index}
+            />
+          ))}
+        </div>
       </div>
+      {/* <div className="absolute -z-10 h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] opacity-5 [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div> */}
+      {/* <div className="absolute inset-0 left-0 top-0 -z-10 h-full w-full  bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div> */}
     </div>
   );
 }
