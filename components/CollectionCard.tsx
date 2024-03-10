09a698775b943ef5876bcb6a74ce4c961e01f1da
aydosjs <aji3385@gmail.com>
2024-03-11 00:02:56 +0500

    ui change

--- components/CollectionCard.tsx
+++ components/CollectionCard.tsx
@@ -10,13 +10,14 @@ export default function CollectionCard(item: Collection, colSpan: string) {
       className={`
       ${colSpan}
       group
-      relative min-h-60 cursor-pointer overflow-hidden rounded-md border-2 border-slate-300 bg-cover bg-center bg-no-repeat transition-all duration-300 dark:border-slate-800`}
+      relative min-h-60 cursor-pointer overflow-hidden rounded-md border-2 border-slate-300 bg-cover bg-center bg-no-repeat transition-all duration-300 dark:border-slate-800 dark:bg-black`}
     >
       {/* <div className="absolute top-0 left-0 inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> */}
-      <div className="absolute left-0 top-0 flex h-full w-full items-end p-4 transition-all duration-300 hover:bg-slate-800/70 dark:bg-slate-800/30 dark:group-hover:bg-slate-800/70">
-        <div className="flex flex-col space-y-2">
-          <h1 className="text-xl text-slate-100">{item.name}</h1>
-          <p className="text-md line-clamp-3 text-slate-400">
+      <div className="absolute bottom-0 left-0 h-24 w-full backdrop-blur transition-all duration-100 group-hover:h-full dark:bg-black/50"></div>
+      <div className="absolute left-0 top-0 flex h-full w-full items-end p-4 transition-all duration-300">
+        <div className="flex flex-col space-y-1 transition-all duration-300">
+          <h1 className="text-lg text-slate-50">{item.name}</h1>
+          <p className="line-clamp-2 text-sm text-slate-300 transition-all duration-300 group-hover:line-clamp-5">
             {item.description}
           </p>
         </div>
