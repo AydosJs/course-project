dd5a64d59612510f388d9fd4ed134874d12c77a0
aydosjs <aji3385@gmail.com>
2024-03-12 17:21:47 +0500

    bug_fix

--- app/api/user/update/route.ts
+++ app/api/user/update/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function PATCH(request: Request, res: Response) {
+export async function PATCH(request: Request): Promise<Response> {
   const { id, email, name } = await request.json();
 
   try {
@@ -26,9 +26,15 @@ export async function PATCH(request: Request, res: Response) {
       );
     }
 
-    return null;
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 },
+    );
   } catch (error) {
     console.error("Error updating user:", error);
-    return null; // Or throw a specific error for handling at the caller level
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 },
+    );
   }
 }
