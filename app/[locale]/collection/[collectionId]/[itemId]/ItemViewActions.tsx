69b20ca5b0757d8feac78d95cb459dc26141495a
aydosjs <aji3385@gmail.com>
2024-03-28 02:03:58 +0500

    bg fix

--- app/[locale]/collection/[collectionId]/[itemId]/ItemViewActions.tsx
+++ app/[locale]/collection/[collectionId]/[itemId]/ItemViewActions.tsx
@@ -54,7 +54,7 @@ export default function ItemViewActions({ item }: { item: Item }) {
         toast.success("Item deleted successfully", {
           id: "successDeleting",
         });
-        router.refresh();
+        router.push(`/collection/${item.collectionId}`);
       } else {
         toast.error("Error deleting Item", {
           id: "errorDeleting",
